808165a09293511f40d2d98201a6d8c2ac6f4ad5
sage99 <vk97104@gmail.com>
2019-02-05 01:05:06 +0530

    new production build

--- dist/precache-manifest.eaaf0e34b6d0692b8535c8e67793b096.js
+++ dist/precache-manifest.eaaf0e34b6d0692b8535c8e67793b096.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "35b07eb2f8711ae08d1f58c043880930",
-    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
@@ -24,16 +24,16 @@ self.__precacheManifest = [
     "url": "/img/heroes.76c7dd16.gif"
   },
   {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
     "revision": "570eb83859dc23dd0eec423a49e147fe",
     "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
   },
   {
     "revision": "012cf6a10129e2275d79d6adac7f3b02",
     "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
-  },
-  {
-    "revision": "e79bfd88537def476913f3ed52f4f4b3",
-    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
   },
   {
     "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
@@ -68,20 +68,20 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
   },
   {
-    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
-    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
-  },
-  {
     "revision": "13efe6cbc10b97144a28310ebdeda594",
     "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
   },
   {
-    "revision": "4357beb823a5f8d65c260f045d9e019a",
-    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
   },
   {
-    "revision": "0785a2ee975f1fbdc21d",
-    "url": "/js/app.9143e5b8.js"
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "0f4553c5a5f1844eabe4",
+    "url": "/js/app.797d37b3.js"
   },
   {
     "revision": "5decb835656b24ce3a6c",
@@ -100,24 +100,24 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
   },
   {
-    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
-    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
-  },
-  {
     "revision": "83e114c316fcc3f23f524ec3e1c65984",
     "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
   },
   {
     "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
     "url": "/fonts/Roboto-Bold.50d75e48.woff"
   },
   {
-    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
-    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
-  },
-  {
     "revision": "59eb3601394dd87f30f82433fb39dd94",
     "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
   },
   {
     "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
@@ -136,12 +136,12 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
   },
   {
-    "revision": "ee89298c89b600293d02770ca164e011",
+    "revision": "b06689335364050f2a2d2d358ffe76da",
     "url": "/index.html"
   },
   {
-    "revision": "0785a2ee975f1fbdc21d",
-    "url": "/css/app.f41347ec.css"
+    "revision": "0f4553c5a5f1844eabe4",
+    "url": "/css/app.31bf3bcd.css"
   },
   {
     "revision": "050e6c5eab93e50ee67296edd373ac14",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c38af3baac1c7322413950437fdd8fee.js"
+  "/precache-manifest.eaaf0e34b6d0692b8535c8e67793b096.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "cine-flick"});
